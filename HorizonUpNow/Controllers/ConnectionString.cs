6d552baa5164d421d0a177eb6a89441738ca775e
Boby <44772249+bori0409@users.noreply.github.com>
2019-12-03 13:25:43 +0000

    Model Classses + GET GET{id} POST

---
+++ HorizonUpNow/Controllers/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorizonUpNow.Controllers
+{
+    public class ConnectionString
+    {
+        public static readonly string connectionStrings = "Server=tcp:motionsservervab.database.windows.net,1433;Initial Catalog=VAB;Persist Security Info=False;User ID=VaAlBo;Password=teamVab2019;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    }
+}
