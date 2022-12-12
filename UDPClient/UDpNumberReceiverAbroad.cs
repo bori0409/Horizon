b13bde9a2c79ed342c019909732658d85383c08c
Boby <44772249+bori0409@users.noreply.github.com>
2022-12-12 11:47:18 +0100

    yes working udp

--- UDPClient/UDpNumberReceiverAbroad.cs
+++ UDPClient/UDpNumberReceiverAbroad.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace UDPClient
 {
@@ -13,49 +17,90 @@ namespace UDPClient
     class UDPNumberReceiverAbroad
 
     {
-        static void Main(string[] args)
+        
+
+        private const int Port = 4210;
+        
+
+
+
+
+
+        static void Main()
         {
-            //Creates a UdpClient for reading incoming data.
-
-            UdpClient udpReceiver = new UdpClient(7000);
-
-            //Creates an IPEndPoint to record the IP Address and port number of the sender.
-            //This IPEndPoint will allow you to read datagrams sent from a specific ip-source on port 7000
-            // 192.168.3.224/127.0.0.1 
-            // IPAddress ip = IPAddress.Parse("127.0.0.1");
-            // IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 7000);
-
-
-            //BROADCASTING RECEIVER
-            //This IPEndPoint will allow you to read datagrams sent from any ip-source on port 7000
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 7000);
-
-            //IPEndPoint object will allow us to read datagrams sent from any source.
-            //IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-
-            // Blocks until a message returns on this socket from a remote host.
-            Console.WriteLine("Receiver is blocked");
-            try
+            IPAddress ip = IPAddress.Parse("172.20.10.4");
+            using (UdpClient socket = new UdpClient(new IPEndPoint(ip, Port)))
             {
+                IPEndPoint remoteEndPoint = new IPEndPoint(ip, 0);
+
+
+
                 while (true)
                 {
-                    Byte[] receiveBytes = udpReceiver.Receive(ref RemoteIpEndPoint);
+                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
 
-                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                    Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
+                        remoteEndPoint.Address, remoteEndPoint.Port, message);
 
-                    Console.WriteLine("Sender: " + receivedData.ToString());
-                    Console.WriteLine("This message was sent from " +
-                                                RemoteIpEndPoint.Address.ToString() +
-                                                " on their port number " +
-                                                RemoteIpEndPoint.Port.ToString());
-                    Thread.Sleep(200);
 
+                    var calc = WindowFinder.FindWindow("Calculator");
+                    WindowFinder.SetForegroundWindow(calc);
+                   var number = float.Parse(message, CultureInfo.InvariantCulture.NumberFormat);
+                    
+                    if (number < -0.6)
+                    {
+                        Console.WriteLine("LEFT");
+                       SendKeys.SendWait("1");
+                    }
+                    else if (number > 0.6)
+                    {
+                        Console.WriteLine("RIGHT");
+                        //
+                        SendKeys.SendWait("2");
+                    }
+                    else
+                        {
+                        Console.WriteLine("WAITINGG.........");
+                        // SendKeys.SendWait("") 
+                    }
+                        /*
+                        SendKeys.SendWait("111");
+                        SendKeys.SendWait("*");
+                        SendKeys.SendWait("11");
+                        SendKeys.SendWait("=");
+                        Console.WriteLine("Calculator done");
+                        */
+
+                    }
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
         }
+
+
+    }
+    public class WindowFinder
+    {
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+        public static IntPtr FindWindow(string caption)
+        {
+
+            return FindWindow(null, caption);
+
+
+
+
+
+        }
+
+
+
+
     }
 }
